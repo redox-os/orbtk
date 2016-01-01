8fc88a7dd620df7f8f34ad6386fbd4a35824a227
polymetric <polymetricofficial@gmail.com>
2016-01-01 12:12:02 -0500

    fixup

--- src/lib.rs
+++ src/lib.rs
@@ -47,64 +47,64 @@ pub fn example() {
     let mut y = 10;
 
     let label = Label::new()
-                    .position(x, y)
-                    .size(400, 16)
-                    .text("Test Label")
-                    .place(&mut window);
+        .position(x, y)
+        .size(400, 16)
+        .text("Test Label")
+        .place(&mut window);
 
     y += label.rect.get().height as i32 + 10;
 
     let text_box = TextBox::new()
-                       .position(x, y)
-                       .size(342, 16)
-                       .on_enter(move |text_box: &TextBox| {
-                           label.text.set(text_box.text.get());
-                       })
-                       .place(&mut window);
+        .position(x, y)
+        .size(342, 16)
+        .on_enter(move |text_box: &TextBox| {
+            label.text.set(text_box.text.get());
+        })
+        .place(&mut window);
 
     let button = Button::new()
-                     .position(x + text_box.rect.get().width as i32 + 10, y)
-                     .size(48, text_box.rect.get().height)
-                     .text("Update")
-                     .on_click(move |_button: &Button, _point: Point| {
-                         text_box.emit_enter();
-                     })
-                     .place(&mut window);
+        .position(x + text_box.rect.get().width as i32 + 10, y)
+        .size(48, text_box.rect.get().height)
+        .text("Update")
+        .on_click(move |_button: &Button, _point: Point| {
+            text_box.emit_enter();
+        })
+        .place(&mut window);
 
     y += button.rect.get().height as i32 + 10;
 
     let progress_label = Label::new()
-                             .text("Progress: 0%")
-                             .position(x, y)
-                             .size(400, 16)
-                             .place(&mut window);
+        .text("Progress: 0%")
+        .position(x, y)
+        .size(400, 16)
+        .place(&mut window);
 
     y += progress_label.rect.get().height as i32 + 10;
 
     let progress_bar = ProgressBar::new()
-                           .position(x, y)
-                           .size(400, 16)
-                           .on_click(move |progress_bar: &ProgressBar, point: Point| {
-                               let progress = point.x * 100 / progress_bar.rect.get().width as i32;
-                               progress_label.text.set(format!("Progress: {}%", progress));
-                               progress_bar.value.set(progress);
-                           })
-                           .place(&mut window);
+        .position(x, y)
+        .size(400, 16)
+        .on_click(move |progress_bar: &ProgressBar, point: Point| {
+            let progress = point.x * 100 / progress_bar.rect.get().width as i32;
+            progress_label.text.set(format!("Progress: {}%", progress));
+            progress_bar.value.set(progress);
+        })
+        .place(&mut window);
 
     y += progress_bar.rect.get().height as i32 + 10;
 
     let multi_line_label = Label::new()
-                               .text("Multi-Line Text")
-                               .position(x, y)
-                               .size(400, 16)
-                               .place(&mut window);
+        .text("Multi-Line Text")
+        .position(x, y)
+        .size(400, 16)
+        .place(&mut window);
 
     y += multi_line_label.rect.get().height as i32 + 10;
 
     let multi_line_text_box = TextBox::new()
-                                  .position(x, y)
-                                  .size(400, 128)
-                                  .place(&mut window);
+        .position(x, y)
+        .size(400, 128)
+        .place(&mut window);
 
     y += multi_line_text_box.rect.get().height as i32 + 10;
 
