87db00f3699052283a608e373a9543c634cea17d
stratact <stratact1@gmail.com>
2016-03-20 08:58:33 -0700

    Refactor `Place` trait to be less repetitive

--- src/button.rs
+++ src/button.rs
@@ -63,22 +63,8 @@ impl Click for Button {
 }
 
 impl Place for Button {
-    fn position(self, x: i32, y: i32) -> Self {
-        let mut rect = self.rect.get();
-        rect.x = x;
-        rect.y = y;
-        self.rect.set(rect);
-
-        self
-    }
-
-    fn size(self, width: u32, height: u32) -> Self {
-        let mut rect = self.rect.get();
-        rect.width = width;
-        rect.height = height;
-        self.rect.set(rect);
-
-        self
+    fn rect(&self) -> &CopyCell<Rect> {
+        &self.rect
     }
 }
 
--- src/text_box.rs
+++ src/text_box.rs
@@ -76,22 +76,8 @@ impl Enter for TextBox {
 }
 
 impl Place for TextBox {
-    fn position(self, x: i32, y: i32) -> Self {
-        let mut rect = self.rect.get();
-        rect.x = x;
-        rect.y = y;
-        self.rect.set(rect);
-
-        self
-    }
-
-    fn size(self, width: u32, height: u32) -> Self {
-        let mut rect = self.rect.get();
-        rect.width = width;
-        rect.height = height;
-        self.rect.set(rect);
-
-        self
+    fn rect(&self) -> &CopyCell<Rect> {
+        &self.rect
     }
 }
 
