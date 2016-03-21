16751d91292359f1ad3c2b6cbf9d34c3664a6949
stratact <stratact1@gmail.com>
2016-03-20 17:16:25 -0700

    Update `Place` impl for `Action` and `Menu`

--- src/menu.rs
+++ src/menu.rs
@@ -69,6 +69,12 @@ impl Widget for Menu {
     }
 }
 
+impl Place for Menu {
+    fn rect(&self) -> &CopyCell<Rect> {
+        &self.rect
+    }
+}
+
 impl Action {
     pub fn new(text: &str) -> Self {
         Action {
@@ -89,22 +95,8 @@ impl Action {
 }
 
 impl Place for Action {
-    fn position(self, x: i32, y: i32) -> Self {
-        let mut rect = self.rect.get();
-        rect.x = x;
-        rect.y = y;
-        self.rect.set(rect);
-
-        self
-    }
-
-    fn size(self, width: u32, height: u32) -> Self {
-        let mut rect = self.rect.get();
-        rect.width = width;
-        rect.height = height;
-        self.rect.set(rect);
-
-        self
+    fn rect(&self) -> &CopyCell<Rect> {
+        &self.rect
     }
 }
 
