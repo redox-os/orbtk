ac3eefd8eba8e361f7acaef56bf8165b9b386623
stratact <stratact1@gmail.com>
2016-08-21 13:13:32 -0700

    Implement WidgetCore to share the same fields that each widget uses

--- src/widget.rs
+++ src/widget.rs
@@ -1,6 +1,23 @@
-use super::{Event, Renderer};
+use super::{Color, Event, Rect, Renderer};
 
 use std::any::Any;
+use std::cell::Cell;
+
+pub struct WidgetCore {
+    pub rect: Cell<Rect>,
+    pub bg: Color,
+    pub fg: Color,
+}
+
+impl WidgetCore {
+    pub fn new(bg: Color, fg: Color) -> Self {
+        WidgetCore {
+            rect: Cell::new(Rect::default()),
+            bg: bg,
+            fg: fg,
+        }
+    }
+}
 
 pub trait Widget : Any {
     fn draw(&self, renderer: &mut Renderer, focused: bool);
