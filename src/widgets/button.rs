0d548bc50a91a7aaa7c3c231ecdba49d62b20e07
Isabelle Knott <isabelle@blackle-mori.com>
2018-01-04 10:09:40 -0500

    Unpress buttons regardless of left_button state

--- src/widgets/button.rs
+++ src/widgets/button.rs
@@ -131,10 +131,8 @@ impl Widget for Button {
                         *redraw = true;
                     }
 
-                    if !left_button {
-                        if self.pressed.check_set(false) {
-                            *redraw = true;
-                        }
+                    if self.pressed.check_set(false) {
+                        *redraw = true;
                     }
                 }
 
