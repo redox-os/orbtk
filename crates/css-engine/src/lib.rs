c841593a679fa7bf9b70a52157e8c39c8d7a3262
Florian Blasius <flovanpt@posteo.de>
2019-05-14 19:48:07 +0200

    Add description to theme crate.

--- crates/css-engine/src/lib.rs
+++ crates/css-engine/src/lib.rs
@@ -1,3 +1,25 @@
+/*!
+
+This crate provides functions to load a css files as theme and access it properties with
+selectors. 
+
+This crate depends on the orbtk_utils crate.
+
+# Example
+
+Basic usage of the tree:
+
+```rust,no_run
+
+use orbtk_css_engine::prelude::*;
+
+let mut theme = Theme::create_from_path("theme.css").build();
+let selector = Selector::from("button");
+let background = theme.brush("background", &selector);
+
+```
+
+ */
 
 pub use selector::*;
 pub use theme::*;
