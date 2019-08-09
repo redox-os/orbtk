6b075e159777075b8b8f7fc4becbdbf09e748791
Florian Blasius <flovanpt@posteo.de>
2019-08-09 15:54:45 +0200

    Widget exampel update.

--- examples/widgets.rs
+++ examples/widgets.rs
@@ -16,6 +16,8 @@ impl Default for MainViewState {
                 "Item 2".to_string(),
                 "Item 3".to_string(),
                 "Last Item".to_string(),
+                "Last Item".to_string(),
+                "Last Item".to_string(),
             ],
         }
     }
@@ -53,8 +55,8 @@ widget!(
 impl Template for MainView {
     fn template(self, id: Entity, context: &mut BuildContext) -> Self {
         let state = self.clone_state();
-
         let list_state = self.clone_state();
+        let list_count = list_state.list.len();
 
         self.name("MainView").count_text("Button count: 0").child(
             Grid::create()
@@ -62,107 +64,108 @@ impl Template for MainView {
                 .columns(
                     Columns::create()
                         .column("Auto")
-                        .column(32.0)
+                        .column(16.0)
                         .column("Auto")
-                        .column(32.0)
-                        .column(120.0)
-                        .column("*")
+                        .column(16.0)
+                        .column("Auto")
                         .build(),
                 )
-                .rows(
-                    Rows::create()
-                        .row("Auto")
-                        .row("Auto")
-                        .row("Auto")
-                        .row("Auto")
-                        .row("Auto")
-                        .row("Auto")
-                        .row("*")
-                        .build(),
-                )
-                // Column 0
-                .child(create_header(context, "Button", 0, 0))
                 .child(
-                    Button::create()
-                        .text("Button")
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .icon(material_font_icons::CHECK_FONT_ICON)
+                    Stack::create()
                         .attach(GridColumn(0))
-                        .attach(GridRow(1))
-                        .on_click(move |_| {
-                            state.increment();
-                            true
-                        })
-                        .build(context),
-                )
-                .child(
-                    Button::create()
-                        .text("Primary")
-                        .selector(SelectorValue::new().with("button").class("primary"))
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .icon(material_font_icons::CHECK_FONT_ICON)
-                        .attach(GridColumn(0))
-                        .attach(GridRow(2))
-                        .build(context),
-                )
-                .child(
-                    ToggleButton::create()
-                        .text("ToggleButton")
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .attach(GridColumn(0))
-                        .attach(GridRow(3))
-                        .build(context),
-                )
-                .child(
-                    CheckBox::create()
-                        .text("CheckBox")
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .attach(GridColumn(0))
-                        .attach(GridRow(4))
-                        .build(context),
-                )
-                .child(
-                    Switch::create()
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .attach(GridColumn(0))
-                        .attach(GridRow(5))
-                        .build(context),
-                )
-                // Column 2
-                .child(create_header(context, "Text", 2, 0))
-                .child(
-                    TextBlock::create()
-                        .selector(SelectorValue::new().class("body"))
-                        .text(id)
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .attach(GridColumn(2))
-                        .attach(GridRow(1))
-                        .build(context),
-                )
-                .child(
-                    TextBox::create()
-                        .placeholder("TextBox...")
-                        .text("")
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .attach(GridColumn(2))
-                        .attach(GridRow(2))
-                        .build(context),
-                )
-                // Column 4
-                .child(create_header(context, "Items", 4, 0))
-                .child(
-                    ItemsWidget::create()
-                        .max_height(192.0)
-                        .margin((0.0, 8.0, 0.0, 0.0))
-                        .items_builder(move |bc, index| {
+                      
+                        // Column 0
+                        .child(create_header(context, "Button", 0, 0))
+                        .child(
                             Button::create()
-                                .text(list_state.list[index].as_str())
-                                .build(bc)
-                        })
-                        .items_count(4)
+                                .text("Button")
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .icon(material_font_icons::CHECK_FONT_ICON)
+                                .attach(GridColumn(0))
+                                .attach(GridRow(1))
+                                .on_click(move |_| {
+                                    state.increment();
+                                    true
+                                })
+                                .build(context),
+                        )
+                        .child(
+                            Button::create()
+                                .text("Primary")
+                                .selector(SelectorValue::new().with("button").class("primary"))
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .icon(material_font_icons::CHECK_FONT_ICON)
+                                .attach(GridColumn(0))
+                                .attach(GridRow(2))
+                                .build(context),
+                        )
+                        .child(
+                            ToggleButton::create()
+                                .text("ToggleButton")
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .attach(GridColumn(0))
+                                .attach(GridRow(3))
+                                .build(context),
+                        )
+                        .child(
+                            CheckBox::create()
+                                .text("CheckBox")
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .attach(GridColumn(0))
+                                .attach(GridRow(4))
+                                .build(context),
+                        )
+                        .child(
+                            Switch::create()
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .attach(GridColumn(0))
+                                .attach(GridRow(5))
+                                .build(context),
+                        )
+                      
+                        .build(context),
+                )
+                .child(
+                    Stack::create()
+                    .attach(GridColumn(2))
+                        .child(create_header(context, "Text", 2, 0))
+                        .child(
+                            TextBlock::create()
+                                .selector(SelectorValue::new().class("body"))
+                                .text(id)
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .attach(GridColumn(2))
+                                .attach(GridRow(1))
+                                .build(context),
+                        )
+                        .child(
+                            TextBox::create()
+                                .placeholder("TextBox...")
+                                .text("")
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .attach(GridColumn(2))
+                                .attach(GridRow(2))
+                                .build(context),
+                        ).build(context)
+                )
+                .child(
+                    Grid::create()
+                        .rows(Rows::create().row("Auto").row("*").build())
                         .attach(GridColumn(4))
-                        .attach(GridRow(1))
-                        .attach(RowSpan(5))
+                        .child(create_header(context, "Items", 0, 0))
+                        .child(
+                            ItemsWidget::create()
+                                .attach(GridRow(1))
+                                .vertical_alignment("Start")
+                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .items_builder(move |bc, index| {
+                                    Button::create()
+                                        .text(list_state.list[index].as_str())
+                                        .build(bc)
+                                })
+                                .items_count(list_count)
+                                .build(context),
+                        )
                         .build(context),
                 )
                 .build(context),
