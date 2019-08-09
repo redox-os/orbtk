b5a593487b326bb63b5211dd177d371e4b317690
Florian Blasius <flovanpt@posteo.de>
2019-08-09 20:01:37 +0200

    Work on widget example state refactoring.

--- crates/theme/src/vector_graphics/material_font_icons.rs
+++ crates/theme/src/vector_graphics/material_font_icons.rs
@@ -1,3 +1,5 @@
 /// List of material font icons. Use this to generate a font Icon.
 pub static FLOPPY_FONT_ICON: &str = "";
 pub static CHECK_FONT_ICON: &str = "";
+pub static ADD_FONT_ICON: &str = "";
+pub static MINUS_FONT_ICON: &str = "";
--- examples/widgets.rs
+++ examples/widgets.rs
@@ -2,9 +2,17 @@ use std::cell::Cell;
 
 use orbtk::prelude::*;
 
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    AddItem,
+    RemoveItem,
+    IncrementCounter,
+}
+
 pub struct MainViewState {
     counter: Cell<i32>,
     list: Vec<String>,
+    action: Cell<Option<Action>>,
 }
 
 impl Default for MainViewState {
@@ -15,34 +23,42 @@ impl Default for MainViewState {
                 "Item 1".to_string(),
                 "Item 2".to_string(),
                 "Item 3".to_string(),
-                "Last Item".to_string(),
-                "Last Item".to_string(),
-                "Last Item".to_string(),
             ],
+            action: Cell::new(None),
         }
     }
 }
 
 impl MainViewState {
-    fn increment(&self) {
-        self.counter.set(self.counter.get() + 1)
+    fn action(&self, action: impl Into<Option<Action>>) {
+        self.action.set(action.into());
     }
 }
 
 impl State for MainViewState {
     fn update(&self, context: &mut Context<'_>) {
-        if let Some(button_count_text) = context.widget().try_get_mut::<Text>() {
-            button_count_text.0 = String16::from(format!("Button count: {}", self.counter.get()));
+        if let Some(action) = self.action.get() {
+            match action {
+                Action::AddItem => (),
+                Action::RemoveItem => (),
+                Action::IncrementCounter => {
+                    self.counter.set(self.counter.get() + 1);
+                    if let Some(button_count_text) = context.widget().try_get_mut::<Text>() {
+                        button_count_text.0 =
+                            String16::from(format!("Button count: {}", self.counter.get()));
+                    }
+                }
+            }
+
+            self.action.set(None);
         }
     }
 }
 
-fn create_header(context: &mut BuildContext, text: &str, grid: usize, column: usize) -> Entity {
+fn create_header(context: &mut BuildContext, text: &str) -> Entity {
     TextBlock::create()
         .text(text)
         .selector(SelectorValue::new().with("text-block").class("h1"))
-        .attach(GridColumn(grid))
-        .attach(GridRow(column))
         .build(context)
 }
 
@@ -73,9 +89,8 @@ impl Template for MainView {
                 .child(
                     Stack::create()
                         .attach(GridColumn(0))
-                      
                         // Column 0
-                        .child(create_header(context, "Button", 0, 0))
+                        .child(create_header(context, "Buttons"))
                         .child(
                             Button::create()
                                 .text("Button")
@@ -84,7 +99,7 @@ impl Template for MainView {
                                 .attach(GridColumn(0))
                                 .attach(GridRow(1))
                                 .on_click(move |_| {
-                                    state.increment();
+                                    state.action(Action::IncrementCounter);
                                     true
                                 })
                                 .build(context),
@@ -122,13 +137,12 @@ impl Template for MainView {
                                 .attach(GridRow(5))
                                 .build(context),
                         )
-                      
                         .build(context),
                 )
                 .child(
                     Stack::create()
-                    .attach(GridColumn(2))
-                        .child(create_header(context, "Text", 2, 0))
+                        .attach(GridColumn(2))
+                        .child(create_header(context, "Text"))
                         .child(
                             TextBlock::create()
                                 .selector(SelectorValue::new().class("body"))
@@ -146,24 +160,60 @@ impl Template for MainView {
                                 .attach(GridColumn(2))
                                 .attach(GridRow(2))
                                 .build(context),
-                        ).build(context)
+                        )
+                        .build(context),
                 )
                 .child(
                     Grid::create()
-                        .rows(Rows::create().row("Auto").row("*").build())
+                        .rows(Rows::create().row("Auto").row("Auto").row("Auto").build())
+                        .columns(
+                            Columns::create()
+                                .column("*")
+                                .column(4.0)
+                                .column("*")
+                                .build(),
+                        )
                         .attach(GridColumn(4))
-                        .child(create_header(context, "Items", 0, 0))
+                        .child(
+                            TextBlock::create()
+                                .text("Items")
+                                .selector(SelectorValue::new().with("text-block").class("h1"))
+                                .attach(GridColumn(0))
+                                .attach(ColumnSpan(3))
+                                .attach(GridRow(0))
+                                .build(context),
+                        )
                         .child(
                             ItemsWidget::create()
+                                .padding((4.0, 4.0, 4.0, 2.0))
+                                .attach(GridColumn(0))
+                                .attach(ColumnSpan(3))
                                 .attach(GridRow(1))
                                 .vertical_alignment("Start")
-                                .margin((0.0, 8.0, 0.0, 0.0))
+                                .margin((0.0, 8.0, 0.0, 8.0))
                                 .items_builder(move |bc, index| {
                                     Button::create()
+                                        .margin((0.0, 0.0, 0.0, 2.0))
                                         .text(list_state.list[index].as_str())
                                         .build(bc)
                                 })
                                 .items_count(list_count)
+                                .build(context),
+                        )
+                        .child(
+                            Button::create()
+                                .icon(material_font_icons::MINUS_FONT_ICON)
+                                .min_width(0.0)
+                                .attach(GridColumn(0))
+                                .attach(GridRow(2))
+                                .build(context),
+                        )
+                        .child(
+                            Button::create()
+                                .icon(material_font_icons::ADD_FONT_ICON)
+                                .min_width(0.0)
+                                .attach(GridColumn(2))
+                                .attach(GridRow(2))
                                 .build(context),
                         )
                         .build(context),
