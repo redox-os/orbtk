e688b89e60f803a4b4be202caa97f95bcd02dff1
Florian Blasius <flovanpt@posteo.de>
2019-11-06 19:57:07 +0100

    Add new font icons.

--- crates/theme/src/vector_graphics/material_font_icons.rs
+++ crates/theme/src/vector_graphics/material_font_icons.rs
@@ -3,3 +3,7 @@ pub const FLOPPY_FONT_ICON: &str = "";
 pub const CHECK_FONT_ICON: &str = "";
 pub const ADD_FONT_ICON: &str = "";
 pub const MINUS_FONT_ICON: &str = "";
+pub const LOAD_FONT_ICON: &str = "";
+pub const SAVE_FONT_ICON: &str = "";
+pub const KEYBOARD_ARROW_LEFT_FONT_ICON: &str = "";
+pub const KEYBOARD_ARROW_RIGHT_FONT_ICON: &str = "";
