f501f3b93650fe0d1bb488defe3d32ac267b3225
Florian Blasius <flovanpt@posteo.de>
2019-11-08 19:32:59 +0100

    Add i32, i64 to property source.

--- crates/api/src/properties/mod.rs
+++ crates/api/src/properties/mod.rs
@@ -83,6 +83,8 @@ into_property_source!(usize);
 into_property_source!(u32);
 into_property_source!(f32);
 into_property_source!(f64: i32, f32);
+into_property_source!(i32);
+into_property_source!(i64);
 
 // Implementation of PropertySource for utils types
 into_property_source!(utils::Alignment: &str);
