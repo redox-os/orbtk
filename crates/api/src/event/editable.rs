f7b21e61c019b463c959808fa038135ef2cb1380
Michael Aaron Murphy <mmstick@pm.me>
2019-11-21 10:41:06 -0700

    improvement(events): Create trigger_event macro for simple trigger-based events

--- crates/api/src/event/editable.rs
+++ crates/api/src/event/editable.rs
@@ -3,36 +3,4 @@ use crate::prelude::*;
 
 use std::rc::Rc;
 
-pub struct ChangedEvent(pub Entity);
-
-impl Event for ChangedEvent {}
-
-pub type ChangedHandlerFn = dyn Fn(Entity) + 'static;
-
-pub struct ChangedEventHandler(Rc<ChangedHandlerFn>);
-
-impl EventHandler for ChangedEventHandler {
-    fn handle_event(&self, event: &EventBox) -> bool {
-        if let Ok(event) = event.downcast_ref::<ChangedEvent>() {
-            (self.0)(event.0);
-        }
-
-        false
-    }
-
-    fn handles_event(&self, event: &EventBox) -> bool {
-        event.is_type::<ChangedEvent>()
-    }
-}
-
-impl From<ChangedEventHandler> for Rc<dyn EventHandler> {
-    fn from(event: ChangedEventHandler) -> Self {
-        Rc::new(event)
-    }
-}
-
-pub trait ChangedHandler: Sized + Widget {
-    fn on_changed<H: Fn(Entity) + 'static>(self, handler: H) -> Self {
-        self.insert_handler(ChangedEventHandler(Rc::new(handler)))
-    }
-}
+crate::trigger_event!(ChangedEvent, ChangedEventHandler, ChangedHandler, on_changed);
