a452a57537d1546eb5cd18e8083fe2dc26a6da27
Ralf Zerres <ralf.zerres@networkx.de>
2020-06-21 19:25:32 +0200

    fonts: update and documenting new font identifier
    * reference the new project home for OpenSource "material-icons-font"
    * introduce woff2 files
    * TL;DR: why using woff2 format is recommended
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/theme/src/fonts/mod.rs
+++ crates/theme/src/fonts/mod.rs
@@ -1,7 +1,34 @@
-// /// Reference to the material icon font.
-pub const MATERIAL_ICONS_REGULAR_FONT: &[u8] = include_bytes!("MaterialIcons-Regular.ttf");
-pub const ROBOTO_REGULAR_FONT: &[u8] = include_bytes!("Roboto-Regular.ttf");
+/// The 'Material Design Icons' is the official icon set from Google.
+/// The icons are designed under the material design guidelines.
+/// Because the official repository is no longer maintained from Google,
+/// source code is now maintained and hosted at:
+/// https://github.com/material-icons/material-icons-font
+/// Package-Info: package.json
 
+// TL;DR
+// https://stackoverflow.com/questions/11002820/why-should-we-include-ttf-eot-woff-svg-in-a-font-face
+// ... woff2 gets drafted and accepted, which improves the compression
+// leading to even smaller files, along with the ability to load a single font
+// "in parts" so that a font that supports 20 scripts can be stored as "chunks"
+// on disk instead, with browsers automatically able to load the font "in parts"
+// as needed, rather than needing to transfer the entire font up front, further
+// improving the typesetting experience.
+// If you don't want to support IE 8 and lower, and iOS 4 and lower,
+// and android 4.3 or earlier, then you can just use WOFF
+// (and WOFF2, a more highly compressed WOFF, for the newest browsers that support it.)
+
+// Reference to the material icon fonts.
+pub const MATERIAL_ICONS_FONT_TTF: &[u8] = include_bytes!("MaterialIcons.ttf");
+pub const MATERIAL_ICONS_REGULAR_FONT: &[u8] = include_bytes!("MaterialIcons-Regular.ttf");
+
+pub const MATERIAL_ICONS_FONT: &[u8] = include_bytes!("MaterialIcons.woff2");
+pub const MATERIAL_ICONS_OUTLINED_FONT: &[u8] = include_bytes!("MaterialIcons-Outlined.woff2");
+pub const MATERIAL_ICONS_ROUND_FONT: &[u8] = include_bytes!("MaterialIcons-Round.woff2");
+pub const MATERIAL_ICONS_SHARP_FONT: &[u8] = include_bytes!("MaterialIcons-Sharp.woff2");
+pub const MATERIAL_ICONS_TWOTONE_FONT: &[u8] = include_bytes!("MaterialIcons-TwoTone.woff2");
+
+// Reference to the roboto fonts.
+pub const ROBOTO_REGULAR_FONT: &[u8] = include_bytes!("Roboto-Regular.ttf");
 pub const ROBOTO_MEDIUM_FONT: &[u8] = include_bytes!("Roboto-Medium.ttf");
 
 // font sizes
