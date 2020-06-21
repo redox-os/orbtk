76a5f4be375768aa72f510b4c3927e19a55e24a0
Ralf Zerres <ralf.zerres@networkx.de>
2020-06-21 19:31:58 +0200

    widgets/prelude: incorporate the new vector_graphics fonts
    * introduce woff2 fonts
    * keep ttf font as 'material_icons_font_ttf'
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/widgets/src/prelude.rs
+++ crates/widgets/src/prelude.rs
@@ -12,5 +12,17 @@ pub use ecs::*;
 pub use orbtk_api::css_engine::{Selector, Theme};
 pub use orbtk_render::prelude::Image;
 pub use proc_macros::*;
-pub use theme::{colors, default_theme, fonts, light_theme, vector_graphics::material_font_icons};
+pub use theme::{
+    colors,
+    default_theme,
+    fonts,
+    light_theme,
+    vector_graphics::{
+	material_icons_font_ttf,
+	material_icons_font,
+	material_icons_round_font,
+	material_icons_sharp_font,
+	material_icons_twotone_font,
+    }
+};
 pub use utils::*;
