8afd072b7dfb686ec0e13662b7f5f1fb926711bb
Ralf Zerres <ralf.zerres@networkx.de>
2020-06-24 23:39:15 +0200

    theme: vector_graphics: update woff2 usage material_icons_baseline_font
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/theme/src/vector_graphics/mod.rs
+++ crates/theme/src/vector_graphics/mod.rs
@@ -1,6 +1,6 @@
 pub use self::{
     material_icons_font_ttf::*,
-    material_icons_font::*,
+    material_icons_baseline_font::*,
     material_icons_round_font::*,
     material_icons_sharp_font::*,
     material_icons_twotone_font::*,
@@ -10,7 +10,7 @@ pub use self::{
 pub mod material_icons_font_ttf;
 
 // woff2 mappings
-pub mod material_icons_font;
+pub mod material_icons_baseline_font;
 pub mod material_icons_round_font;
 pub mod material_icons_sharp_font;
 pub mod material_icons_twotone_font;
