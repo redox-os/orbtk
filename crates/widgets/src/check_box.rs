802f3c927cf8d01c091fdfec4823271e791be095
Ralf Zerres <ralf.zerres@networkx.de>
2020-06-24 14:01:51 +0200

    widgets: examples update referencing the renamed ttf font
    * checkbox: reference to material_icons_font_ttf
    * combo_box: reference to material_icons_font_ttf
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/widgets/src/check_box.rs
+++ crates/widgets/src/check_box.rs
@@ -7,124 +7,124 @@ widget!(
     /// **CSS element:** `check-box`
     CheckBox: MouseHandler,
     ChangedHandler {
-        /// Sets or shares the background property.
-        background: Brush,
+	/// Sets or shares the background property.
+	background: Brush,
 
-        /// Sets or shares the border radius property.
-        border_radius: f64,
+	/// Sets or shares the border radius property.
+	border_radius: f64,
 
-        /// Sets or shares the border thickness property.
-        border_width: Thickness,
+	/// Sets or shares the border thickness property.
+	border_width: Thickness,
 
-        /// Sets or shares the border brush property.
-        border_brush: Brush,
+	/// Sets or shares the border brush property.
+	border_brush: Brush,
 
-        /// Sets or shares the padding property.
-        padding: Thickness,
+	/// Sets or shares the padding property.
+	padding: Thickness,
 
-        /// Sets or shares the foreground property.
-        foreground: Brush,
+	/// Sets or shares the foreground property.
+	foreground: Brush,
 
-        /// Sets or shares the text property.
-        text: String16,
+	/// Sets or shares the text property.
+	text: String16,
 
-        /// Sets or share the font size property.
-        font_size: f64,
+	/// Sets or share the font size property.
+	font_size: f64,
 
-        /// Sets or shares the font property.
-        font: String,
+	/// Sets or shares the font property.
+	font: String,
 
-        /// Sets or shares the icon property.
-        icon: String,
+	/// Sets or shares the icon property.
+	icon: String,
 
-        /// Sets or shares the icon brush property.
-        icon_brush: Brush,
+	/// Sets or shares the icon brush property.
+	icon_brush: Brush,
 
-        /// Sets or share the icon font size property.
-        icon_size: f64,
+	/// Sets or share the icon font size property.
+	icon_size: f64,
 
-        /// Sets or shares the icon font property.
-        icon_font: String,
+	/// Sets or shares the icon font property.
+	icon_font: String,
 
-        /// Sets or shares the pressed property.
-        pressed: bool,
+	/// Sets or shares the pressed property.
+	pressed: bool,
 
-        /// Sets or shares the selected property.
-        selected: bool
+	/// Sets or shares the selected property.
+	selected: bool
     }
 );
 
 impl Template for CheckBox {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-        self.name("CheckBox")
-            .element("check-box")
-            .selected(false)
-            .height(24.0)
-            .background(colors::LYNCH_COLOR)
-            .border_radius(2.0)
-            .border_width(0.0)
-            .border_brush("transparent")
-            .padding((8.0, 0.0, 8.0, 0.0))
-            .foreground(colors::LINK_WATER_COLOR)
-            .text("")
-            .font_size(fonts::FONT_SIZE_12)
-            .font("Roboto Regular")
-            .icon(material_font_icons::CHECK_FONT_ICON)
-            .icon_font("Material Icons")
-            .icon_size(fonts::ICON_FONT_SIZE_12)
-            .icon_brush(colors::LINK_WATER_COLOR)
-            .pressed(false)
-            .child(
-                MouseBehavior::new()
-                    .pressed(id)
-                    .enabled(id)
-                    .target(id.0)
-                    .child(
-                        SelectionBehavior::new()
-                            .selected(id)
-                            .enabled(id)
-                            .target(id.0)
-                            .child(
-                                Stack::new()
-                                    .orientation("horizontal")
-                                    .spacing(8.0)
-                                    .child(
-                                        Container::new()
-                                            .size(24.0, 24.0)
-                                            .background(id)
-                                            .border_radius(id)
-                                            .border_width(id)
-                                            .border_brush(id)
-                                            .padding(id)
-                                            .opacity(id)
-                                            .child(
-                                                FontIconBlock::new()
-                                                    .v_align("center")
-                                                    .h_align("center")
-                                                    .icon(id)
-                                                    .icon_brush(id)
-                                                    .icon_size(id)
-                                                    .icon_font(id)
-                                                    .opacity(id)
-                                                    .build(ctx),
-                                            )
-                                            .build(ctx),
-                                    )
-                                    .child(
-                                        TextBlock::new()
-                                            .v_align("center")
-                                            .foreground(id)
-                                            .text(id)
-                                            .font_size(id)
-                                            .font(id)
-                                            .opacity(id)
-                                            .build(ctx),
-                                    )
-                                    .build(ctx),
-                            )
-                            .build(ctx),
-                    )
-                    .build(ctx),
-            )
+	self.name("CheckBox")
+	    .element("check-box")
+	    .selected(false)
+	    .height(24.0)
+	    .background(colors::LYNCH_COLOR)
+	    .border_radius(2.0)
+	    .border_width(0.0)
+	    .border_brush("transparent")
+	    .padding((8.0, 0.0, 8.0, 0.0))
+	    .foreground(colors::LINK_WATER_COLOR)
+	    .text("")
+	    .font_size(fonts::FONT_SIZE_12)
+	    .font("Roboto Regular")
+	    .icon(material_icons_font_ttf::CHECK_FONT_ICON)
+	    .icon_font("Material Icons")
+	    .icon_size(fonts::ICON_FONT_SIZE_12)
+	    .icon_brush(colors::LINK_WATER_COLOR)
+	    .pressed(false)
+	    .child(
+		MouseBehavior::new()
+		    .pressed(id)
+		    .enabled(id)
+		    .target(id.0)
+		    .child(
+			SelectionBehavior::new()
+			    .selected(id)
+			    .enabled(id)
+			    .target(id.0)
+			    .child(
+				Stack::new()
+				    .orientation("horizontal")
+				    .spacing(8.0)
+				    .child(
+					Container::new()
+					    .size(24.0, 24.0)
+					    .background(id)
+					    .border_radius(id)
+					    .border_width(id)
+					    .border_brush(id)
+					    .padding(id)
+					    .opacity(id)
+					    .child(
+						FontIconBlock::new()
+						    .v_align("center")
+						    .h_align("center")
+						    .icon(id)
+						    .icon_brush(id)
+						    .icon_size(id)
+						    .icon_font(id)
+						    .opacity(id)
+						    .build(ctx),
+					    )
+					    .build(ctx),
+				    )
+				    .child(
+					TextBlock::new()
+					    .v_align("center")
+					    .foreground(id)
+					    .text(id)
+					    .font_size(id)
+					    .font(id)
+					    .opacity(id)
+					    .build(ctx),
+				    )
+				    .build(ctx),
+			    )
+			    .build(ctx),
+		    )
+		    .build(ctx),
+	    )
     }
 }
--- examples/widgets.rs
+++ examples/widgets.rs
@@ -132,6 +132,10 @@ widget!(
 
 impl Template for MainView {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 9c81a1c... widgets.rs: updates
         self.name("MainView")
             .result("Button count: 0")
             .counter(0)
@@ -188,9 +192,16 @@ impl Template for MainView {
                                 Button::new()
                                     .text("Button")
                                     .margin((0., 8., 0., 0.))
+<<<<<<< HEAD
                                     .icon(material_font_icons::CHECK_FONT_ICON)
                                     .attach(Grid::column(0))
                                     .attach(Grid::row(1))
+=======
+                                    .attach(Grid::column(0))
+                                    .attach(Grid::row(1))
+                                    //.icon(material_icons_font_ttf::CHECK_FONT_ICON)
+                                    .icon(material_icons_font_ttf::MD_CHECK)
+>>>>>>> 9c81a1c... widgets.rs: updates
                                     .on_click(move |states, _| {
                                         state(id, states).action(Action::IncrementCounter);
                                         true
@@ -203,7 +214,11 @@ impl Template for MainView {
                                     .element("button")
                                     .class("primary")
                                     .margin((0., 8., 0., 0.))
+<<<<<<< HEAD
                                     .icon(material_font_icons::CHECK_FONT_ICON)
+=======
+                                    .icon(material_icons_font_ttf::MD_360)
+>>>>>>> 9c81a1c... widgets.rs: updates
                                     .attach(Grid::column(0))
                                     .attach(Grid::row(2))
                                     .build(ctx),
@@ -212,7 +227,12 @@ impl Template for MainView {
                                 ToggleButton::new()
                                     .class("single_content")
                                     .text("ToggleButton")
+<<<<<<< HEAD
                                     .margin((0., 8., 0., 0.))
+=======
+                                    .icon(material_icons_font_ttf::MD_ALARM_ADD)
+                                    .margin((0., 8., 2., 0.))
+>>>>>>> 9c81a1c... widgets.rs: updates
                                     .attach(Grid::column(0))
                                     .attach(Grid::row(3))
                                     .build(ctx),
@@ -297,6 +317,11 @@ impl Template for MainView {
                                 Button::new()
                                     .margin((0., 8., 0., 0.))
                                     .class("single_content")
+<<<<<<< HEAD
+=======
+                                    .icon(material_icons_font_ttf::MD_CLEAR)
+                                    .margin((0., 8., 0., 0.))
+>>>>>>> 9c81a1c... widgets.rs: updates
                                     .text("clear text")
                                     .on_click(move |states, _| {
                                         state(id, states).action(Action::ClearText);
@@ -372,6 +397,7 @@ impl Template for MainView {
                                         let text = bc.get_widget(id).get::<Vec<String>>("list")
                                             [index]
                                             .clone();
+<<<<<<< HEAD
 
                                         Button::new()
                                             .margin((0., 0., 0., 2.))
@@ -448,6 +474,405 @@ impl Template for MainView {
                     )
                     .build(ctx),
             )
+=======
+	self.name("MainView")
+	    .result("Button count: 0")
+	    .counter(0)
+	    .selected_indices(HashSet::new())
+	    .list(vec![
+		"Item 1".to_string(),
+		"Item 2".to_string(),
+		"Item 3".to_string(),
+	    ])
+	    .list_count(3)
+	    .selection_list(vec![
+		"Item 1".to_string(),
+		"Item 2".to_string(),
+		"Item 3".to_string(),
+		"Item 4".to_string(),
+		"Item 5".to_string(),
+		"Item 6".to_string(),
+		"Item 7".to_string(),
+		"Item 8".to_string(),
+		"Item 9".to_string(),
+		"Item 10".to_string(),
+	    ])
+	    .combo_box_list(vec![
+		"CB 1".to_string(),
+		"CB 2".to_string(),
+		"CB 3".to_string(),
+		"CB 4".to_string(),
+		"CB 5".to_string(),
+		"CB 6".to_string(),
+		"CB 7".to_string(),
+		"CB 8".to_string(),
+		"CB 9".to_string(),
+		"CB 10".to_string(),
+	    ])
+	    .selection_list_count(10)
+	    .combo_box_list_count(10)
+	    .child(
+		Grid::new()
+		    .margin(8.)
+		    .columns(
+			Columns::new()
+			    .add(132.)
+			    .add(16.)
+			    .add(132.)
+			    .add(16.)
+			    .add(132.),
+		    )
+		    .child(
+			Stack::new()
+			    .attach(Grid::column(0))
+			    // Column 0
+			    .child(create_header(ctx, "Buttons"))
+			    .child(
+				Button::new()
+				    .text("Button")
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(1))
+				    .icon(material_icons_font_ttf::CHECK_FONT_ICON)
+				    .icon(material_icons_font::MD_CHECK)
+				    .on_click(move |states, _| {
+					state(id, states).action(Action::IncrementCounter);
+					true
+				    })
+				    .build(ctx),
+			    )
+			    .child(
+				Button::new()
+				    .text("Primary")
+				    .element("button")
+				    .class("primary")
+				    .margin((0., 8., 0., 0.))
+				    .icon(material_icons_font_ttf::CHECK_FONT_ICON)
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(2))
+				    .build(ctx),
+			    )
+			    .child(
+				ToggleButton::new()
+				    .class("single_content")
+				    .text("ToggleButton")
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(3))
+				    .build(ctx),
+			    )
+			    .child(
+				CheckBox::new()
+				    .text("CheckBox")
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(4))
+				    .build(ctx),
+			    )
+			    .child(
+				Switch::new()
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(5))
+				    .build(ctx),
+			    )
+			    .child(
+				TextBlock::new()
+				    .margin((0., 8., 0., 0.))
+				    .element("h1")
+				    .id("value_text")
+				    .text("0")
+				    .h_align("center")
+				    .build(ctx),
+			    )
+			    .child(
+				Slider::new()
+				    .on_changed(move |states, entity| {
+					state(id, states).action(Action::ValueChanged(entity));
+				    })
+				    .build(ctx),
+			    )
+			    .build(ctx),
+		    )
+		    .child(
+			Stack::new()
+			    .attach(Grid::column(2))
+			    .child(create_header(ctx, "Text"))
+			    .child(
+				TextBlock::new()
+				    .class("body")
+				    .text(("result", id))
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(2))
+				    .attach(Grid::row(1))
+				    .build(ctx),
+			    )
+			    .child(
+				TextBox::new()
+				    .water_mark("TextBox...")
+				    .text(("text_one", id))
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(2))
+				    .attach(Grid::row(2))
+				    .on_activate(move |states, entity| {
+					state(id, states).action(Action::EntryActivated(entity));
+				    })
+				    .on_changed(move |states, entity| {
+					state(id, states).action(Action::EntryChanged(entity));
+				    })
+				    .build(ctx),
+			    )
+			    .child(
+				TextBox::new()
+				    .water_mark("TextBox...")
+				    .text(("text_two", id))
+				    .margin((0., 8., 0., 0.))
+				    .attach(Grid::column(2))
+				    .attach(Grid::row(2))
+				    .on_activate(move |states, entity| {
+					state(id, states).action(Action::EntryActivated(entity));
+				    })
+				    .on_changed(move |states, entity| {
+					state(id, states).action(Action::EntryChanged(entity));
+				    })
+				    .build(ctx),
+			    )
+			    .child(
+				Button::new()
+				    .margin((0., 8., 0., 0.))
+				    .class("single_content")
+				    .text("clear text")
+				    .on_click(move |states, _| {
+					state(id, states).action(Action::ClearText);
+					true
+				    })
+				    .build(ctx),
+			    )
+			    .child(
+				NumericBox::new()
+				    .margin((-0., 8., 0., 0.))
+				    .max(123.)
+				    .step(0.123)
+				    .val(0.123)
+				    .build(ctx),
+			    )
+			    .build(ctx),
+		    )
+		    .child(
+			Grid::new()
+			    .rows(
+				Rows::new()
+				    .add("auto")
+				    .add(32.)
+				    .add(16.)
+				    .add(204.)
+				    .add("auto")
+				    .add(192.)
+				    .add("auto"),
+			    )
+			    .columns(Columns::new().add("*").add(4.).add("*"))
+			    .attach(Grid::column(4))
+			    .child(
+				TextBlock::new()
+				    .text("Items")
+				    .element("text-block")
+				    .class("h1")
+				    .attach(Grid::column(0))
+				    .attach(Grid::column_span(3))
+				    .attach(Grid::row(0))
+				    .build(ctx),
+			    )
+			    .child(
+				ComboBox::new()
+				    .items_builder(move |bc, index| {
+					let text = bc
+					    .get_widget(id)
+					    .get::<Vec<String>>("combo_box_list")[index]
+					    .clone();
+					TextBlock::new()
+					    .margin((0., 0., 0., 2.))
+					    .v_align("center")
+					    .text(text)
+					    .build(bc)
+				    })
+				    .selected_index(0)
+				    .attach(Grid::column(0))
+				    .attach(Grid::column_span(3))
+				    .attach(Grid::row(1))
+				    .margin((0., 8., 0., 0.))
+				    .count(("combo_box_list_count", id))
+				    .build(ctx),
+			    )
+			    .child(
+				ItemsWidget::new()
+				    .element("items-widget")
+				    .id("items")
+				    .padding((4., 4., 4., 2.))
+				    .attach(Grid::column(0))
+				    .attach(Grid::column_span(3))
+				    .attach(Grid::row(3))
+				    .margin((0., 0., 0., 8.))
+				    .items_builder(move |bc, index| {
+					let text = bc.get_widget(id).get::<Vec<String>>("list")
+					    [index]
+					    .clone();
+
+					Button::new()
+					    .margin((0., 0., 0., 2.))
+					    .text(text)
+					    .build(bc)
+				    })
+				    .count(("list_count", id))
+				    .build(ctx),
+			    )
+			    .child(
+				Button::new()
+				    .element("button")
+				    .class("single_content")
+				    .id("remove-item-button")
+				    .icon(material_icons_font_ttf::MINUS_FONT_ICON)
+				    //.icon(material_icons_font::MD_MINUS)
+				    .on_click(move |states, _| {
+					state(id, states).action(Action::RemoveItem);
+					true
+				    })
+				    .min_width(0.)
+				    .attach(Grid::column(0))
+				    .attach(Grid::row(4))
+				    .build(ctx),
+			    )
+			    .child(
+				Button::new()
+				    .element("button")
+				    .class("single_content")
+				    .id("add-item-button")
+				    .icon(material_icons_font_ttf::ADD_FONT_ICON)
+				    //.icon(material_icons_font::MD_ADD)
+				    .on_click(move |states, _| {
+					state(id, states).action(Action::AddItem);
+					true
+				    })
+				    .min_width(0.)
+				    .attach(Grid::column(2))
+				    .attach(Grid::row(4))
+				    .build(ctx),
+			    )
+			    .child(
+				ListView::new()
+				    .attach(Grid::column(0))
+				    .attach(Grid::column_span(3))
+				    .attach(Grid::row(5))
+				    .selected_indices(id)
+				    .margin((0., 16., 0., 8.))
+				    .items_builder(move |bc, index| {
+					let text = bc
+					    .get_widget(id)
+					    .get::<Vec<String>>("selection_list")[index]
+					    .clone();
+					TextBlock::new()
+					    .margin((0., 0., 0., 2.))
+					    .v_align("center")
+					    .text(text)
+					    .build(bc)
+				    })
+				    .count(("selection_list_count", id))
+				    .build(ctx),
+			    )
+			    .child(
+				// todo: wrong text width????
+				TextBlock::new()
+				    .element("text-block")
+				    .id("selection")
+				    .max_width(120.)
+				    .attach(Grid::column(0))
+				    .attach(Grid::column_span(3))
+				    .attach(Grid::row(6))
+				    .text("Selected:")
+				    .build(ctx),
+			    )
+			    .build(ctx),
+		    )
+		    .build(ctx),
+	    )
+>>>>>>> dc49248... widgets: convert to use the renamed ttf font
+=======
+
+                                        Button::new()
+                                            .margin((0., 0., 0., 2.))
+                                            .text(text)
+                                            .build(bc)
+                                    })
+                                    .count(("list_count", id))
+                                    .build(ctx),
+                            )
+                            .child(
+                                Button::new()
+                                    .element("button")
+                                    .class("single_content")
+                                    .id("remove-item-button")
+                                    .icon(material_icons_font_ttf::MD_REMOVE_CIRCLE)
+                                    .on_click(move |states, _| {
+                                        state(id, states).action(Action::RemoveItem);
+                                        true
+                                    })
+                                    .min_width(0.)
+                                    .attach(Grid::column(0))
+                                    .attach(Grid::row(4))
+                                    .build(ctx),
+                            )
+                            .child(
+                                Button::new()
+                                    .element("button")
+                                    .class("single_content")
+                                    .id("add-item-button")
+                                    .icon(material_icons_font_ttf::MD_ADD_CIRCLE)
+                                    .on_click(move |states, _| {
+                                        state(id, states).action(Action::AddItem);
+                                        true
+                                    })
+                                    .min_width(0.)
+                                    .attach(Grid::column(2))
+                                    .attach(Grid::row(4))
+                                    .build(ctx),
+                            )
+                            .child(
+                                ListView::new()
+                                    .attach(Grid::column(0))
+                                    .attach(Grid::column_span(3))
+                                    .attach(Grid::row(5))
+                                    .selected_indices(id)
+                                    .margin((0., 16., 0., 8.))
+                                    .items_builder(move |bc, index| {
+                                        let text = bc
+                                            .get_widget(id)
+                                            .get::<Vec<String>>("selection_list")[index]
+                                            .clone();
+                                        TextBlock::new()
+                                            .margin((0., 0., 0., 2.))
+                                            .v_align("center")
+                                            .text(text)
+                                            .build(bc)
+                                    })
+                                    .count(("selection_list_count", id))
+                                    .build(ctx),
+                            )
+                            .child(
+                                // todo: wrong text width????
+                                TextBlock::new()
+                                    .element("text-block")
+                                    .id("selection")
+                                    .max_width(120.)
+                                    .attach(Grid::column(0))
+                                    .attach(Grid::column_span(3))
+                                    .attach(Grid::row(6))
+                                    .text("Selected:")
+                                    .build(ctx),
+                            )
+                            .build(ctx),
+                    )
+                    .build(ctx),
+            )
+>>>>>>> 9c81a1c... widgets.rs: updates
     }
 }
 
