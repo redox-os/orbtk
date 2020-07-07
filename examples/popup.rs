1bd1cb61e12703cb2c69d804ae8a2f869df11a27
Florian Blasius <flovanpt@posteo.de>
2020-07-07 11:38:25 +0200

    Format.

--- examples/popup.rs
+++ examples/popup.rs
@@ -5,15 +5,15 @@ static BTN_ID: &'static str = "BUTTON";
 
 #[derive(Copy, Clone)]
 enum PopUpAction {
-   Show,
-   Hide
+    Show,
+    Hide,
 }
 
 #[derive(Default, AsAny)]
 struct MainViewState {
     action: Option<PopUpAction>,
     show_popup: bool,
-    popup: Option<Entity>
+    popup: Option<Entity>,
 }
 
 impl MainViewState {
@@ -35,7 +35,7 @@ impl State for MainViewState {
                     let stack = ctx.entity_of_child(STACK_ID).unwrap();
                     let current_entity = ctx.entity;
                     let build_context = &mut ctx.build_context();
-                    
+
                     let popup = create_popup(current_entity, "Popup text", build_context);
                     build_context.append_child(stack, popup);
                     self.popup = Some(popup);
@@ -63,54 +63,53 @@ fn create_popup(target: Entity, text: &str, build_context: &mut BuildContext) ->
         .height(250.0)
         .child(
             Container::new()
-            .background("#FFFFFF")
-            .border_radius(3.0)
-            .border_width(2.0)
-            .border_brush("#000000")
-            .padding(8.0)
-            .child(
-                TextBlock::new()
-                .h_align("center")
-                .v_align("top")
-                .foreground("#000000")
-                .text(text)
-                .build(build_context)
-            )
-            .build(build_context)
+                .background("#FFFFFF")
+                .border_radius(3.0)
+                .border_width(2.0)
+                .border_brush("#000000")
+                .padding(8.0)
+                .child(
+                    TextBlock::new()
+                        .h_align("center")
+                        .v_align("top")
+                        .foreground("#000000")
+                        .text(text)
+                        .build(build_context),
+                )
+                .build(build_context),
         )
         .build(build_context)
 }
 
 fn change_button_title(title: &str, ctx: &mut Context) {
     let btn = ctx.entity_of_child(BTN_ID).unwrap();
-    ctx.get_widget(btn).set::<String16>("text", String16::from(title));
+    ctx.get_widget(btn)
+        .set::<String16>("text", String16::from(title));
 }
 
 widget!(MainView<MainViewState>);
 
 impl Template for MainView {
     fn template(self, id: Entity, bc: &mut BuildContext) -> Self {
-        self.name("MainView")
-        .margin(16.0)
-        .child(
+        self.name("MainView").margin(16.0).child(
             Stack::new()
-            .id(STACK_ID)
-            .h_align("center")
-            .spacing(16.0)
-            .child(
-                Button::new()
-                .id(BTN_ID)
-                .v_align("top")
+                .id(STACK_ID)
                 .h_align("center")
-                .text("Click me to show popup")
-                .width(250.0)
-                .on_click(move |states, _| -> bool {
-                    states.get_mut::<MainViewState>(id).show_popup();
-                    true
-                })
-                .build(bc))
-               
-            .build(bc)
+                .spacing(16.0)
+                .child(
+                    Button::new()
+                        .id(BTN_ID)
+                        .v_align("top")
+                        .h_align("center")
+                        .text("Click me to show popup")
+                        .width(250.0)
+                        .on_click(move |states, _| -> bool {
+                            states.get_mut::<MainViewState>(id).show_popup();
+                            true
+                        })
+                        .build(bc),
+                )
+                .build(bc),
         )
     }
 }
