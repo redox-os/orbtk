587a99126dd01a0872f0980012b725f156c94724
Michał Sieroń <michalwsieron@gmail.com>
2020-07-14 17:08:01 +0200

    Updated `Rectangle` to use `orbtk_utils::Point`

--- crates/utils/src/rectangle.rs
+++ crates/utils/src/rectangle.rs
@@ -1,25 +1,23 @@
+use crate::Point;
+
 /// Describes a new visual rectangle.
 #[derive(Copy, Clone, Default, Debug, PartialEq)]
 pub struct Rectangle {
-    /// X position of the rectangle.
-    pub x: f64,
-
-    /// Y position of the rectangle.
-    pub y: f64,
+    /// Position of the rectangle.
+    position: Point,
 
     /// Width of the rectangle.
-    pub width: f64,
+    width: f64,
 
     /// Height of the rectangle.
-    pub height: f64,
+    height: f64,
 }
 
 impl Rectangle {
     /// Create a new rectangle with the given parameters.
-    pub fn new(x: f64, y: f64, width: f64, height: f64) -> Self {
+    pub fn new(position: impl Into<Point>, width: f64, height: f64) -> Self {
         Rectangle {
-            x,
-            y,
+            position: position.into(),
             width,
             height,
         }
@@ -27,33 +25,32 @@ impl Rectangle {
 
     /// Gets x.
     pub fn x(&self) -> f64 {
-        self.x
+        self.position.x()
     }
 
     /// Sets x.
     pub fn set_x(&mut self, x: f64) {
-        self.x = x;
+        self.position.set_x(x);
     }
 
     /// Gets y.
     pub fn y(&self) -> f64 {
-        self.y
+        self.position.y()
     }
 
     /// Sets y.
     pub fn set_y(&mut self, y: f64) {
-        self.y = y;
+        self.position.set_y(y);
     }
 
-    /// Gets position with x and y.
-    pub fn position(&self) -> (f64, f64) {
-        (self.x, self.y)
+    /// Gets position as `Point`.
+    pub fn position(&self) -> Point {
+        self.position
     }
 
-    /// Sets position with x and y.
-    pub fn set_position(&mut self, x: f64, y: f64) {
-        self.x = x;
-        self.y = y;
+    /// Sets position.
+    pub fn set_position(&mut self, position: impl Into<Point>) {
+        self.position = position.into();
     }
 
     /// Gets the width.
@@ -88,24 +85,25 @@ impl Rectangle {
     }
 
     /// Check if this rect contains the given `point`.
-    pub fn contains(&self, point: (f64, f64)) -> bool {
-        point.0 >= self.x
-            && point.0 < self.x + self.width
-            && point.1 >= self.y
-            && point.1 < self.y + self.height
+    pub fn contains(&self, point: impl Into<Point>) -> bool {
+        let point: Point = point.into();
+        point.x() >= self.x()
+            && point.x() < self.x() + self.width()
+            && point.y() >= self.y()
+            && point.y() < self.y() + self.height()
     }
 
     pub fn contains_rect(&self, rect: &Rectangle) -> bool {
         let p1 = rect.position();
-        let p2 = (p1.0 + rect.width(), p1.1 + rect.height());
+        let p2 = (p1.x() + rect.width(), p1.y() + rect.height());
         self.contains(p1) && self.contains(p2)
     }
 
     pub fn intersects(&self, rect: &Rectangle) -> bool {
-        !(rect.x() >= (self.x + self.width)
-            || self.x >= (rect.x() + rect.width())
-            || rect.y() >= (self.y + self.height)
-            || self.y >= (rect.y() + rect.height()))
+        !(rect.x() >= (self.x() + self.width())
+            || self.x() >= (rect.x() + rect.width())
+            || rect.y() >= (self.y() + self.height())
+            || self.y() >= (rect.y() + rect.height()))
     }
 }
 
@@ -113,13 +111,13 @@ impl Rectangle {
 
 impl From<(i32, i32, i32, i32)> for Rectangle {
     fn from(t: (i32, i32, i32, i32)) -> Self {
-        Rectangle::new(t.0 as f64, t.1 as f64, t.2 as f64, t.3 as f64)
+        Rectangle::new((t.0 as f64, t.1 as f64), t.2 as f64, t.3 as f64)
     }
 }
 
 impl From<(f64, f64, f64, f64)> for Rectangle {
     fn from(t: (f64, f64, f64, f64)) -> Self {
-        Rectangle::new(t.0, t.1, t.2, t.3)
+        Rectangle::new((t.0, t.1), t.2, t.3)
     }
 }
 
@@ -129,11 +127,11 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let rect = Rectangle::new(5.0, 10.0, 20.0, 30.0);
+        let rect = Rectangle::new((5.0, 10.0), 20.0, 30.0);
 
-        assert_eq!(rect.x, 5.0);
-        assert_eq!(rect.y, 10.0);
-        assert_eq!(rect.width, 20.0);
-        assert_eq!(rect.height, 30.0);
+        assert_eq!(rect.x(), 5.0);
+        assert_eq!(rect.y(), 10.0);
+        assert_eq!(rect.width(), 20.0);
+        assert_eq!(rect.height(), 30.0);
     }
 }
