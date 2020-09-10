3fef5817dbf6c71f29f8ddecb1d176abd29ee412
Florian Blasius <flovanpt@posteo.de>
2020-09-10 13:35:32 +0200

    test with example

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -6,11 +6,43 @@ fn main() {
 
     Application::new()
         .window(|ctx| {
+            let pager = Pager::new()
+                .width(100)
+                .height(100)
+                .child(TextBlock::new().text("test 1").build(ctx))
+                .child(TextBlock::new().text("test 2").build(ctx))
+                .child(TextBlock::new().text("test 3").build(ctx))
+                .margin(4.0)
+                .build(ctx);
             Window::new()
                 .title("OrbTk - minimal example")
                 .position((100.0, 100.0))
                 .size(420.0, 730.0)
-                .child(TextBlock::new().text("OrbTk").margin(4.0).build(ctx))
+                .child(
+                    Stack::new()
+                        .child(
+                            Button::new()
+                                // .enabled(("next_enabled", pager))
+                                .text("next")
+                                .on_click(move |states, _| {
+                                    states.get_mut::<PagerState>(pager).next();
+                                    true
+                                })
+                                .build(ctx),
+                        )
+                        .child(
+                            Button::new()
+                                // .enabled(("previous_enabled", pager))
+                                .text("previous")
+                                .on_click(move |states, _| {
+                                    states.get_mut::<PagerState>(pager).previous();
+                                    true
+                                })
+                                .build(ctx),
+                        )
+                        .child(pager)
+                        .build(ctx),
+                )
                 .build(ctx)
         })
         .run();
