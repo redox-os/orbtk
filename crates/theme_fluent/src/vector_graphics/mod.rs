58c338be445d2d2c83389549e5a7e6062df3c383
Florian Blasius <flovanpt@posteo.de>
2020-11-11 06:19:56 +0100

    removed fonts with license issues

--- crates/theme_fluent/src/vector_graphics/mod.rs
+++
@@ -1,6 +0,0 @@
-pub use self::segmdl2_assets_font::*;
-
-/// Icons in the `Segoe MDL2 Assets font` are accessible via
-/// their unique codepoints, representing the `glyph`. The codepoints
-/// are mapped to this given constants.
-pub mod segmdl2_assets_font;
