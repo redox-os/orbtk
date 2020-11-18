3c03d90327c089dc012d933d11960205fb4e7362
Ralf Zerres <ralf.zerres@networkx.de>
2020-11-18 01:21:10 +0100

    theme_fluent: exchange mdl2 fonttype to `otf`
    * the mdl2 assets font is in line with version 1.1.0
    * just use `oft` fonts
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/theme_fluent/src/fonts.rs
+++ crates/theme_fluent/src/fonts.rs
@@ -1,5 +1,5 @@
 /// The `regular` variant of the `MDL2 Icons` asset font family.
-pub const MDL2_ICONS_FONT: &[u8] = include_bytes!("../assets/fonts/mdl2.ttf");
+pub const MDL2_ICONS_FONT: &[u8] = include_bytes!("../assets/fonts/mdl2.otf");
 
 /// The `regular` variant of the `Selawik UI` font family.
 pub const SELAWIK_REGULAR_FONT: &[u8] = include_bytes!("../assets/fonts/Selawik-Regular.otf");
