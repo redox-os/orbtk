6db21575886d8475122393986e04a0829c422e94
Florian Blasius <flovanpt@posteo.de>
2021-01-20 15:50:44 +0100

    remove miniraq backend

--- crates/render/src/common.rs
+++ crates/render/src/common.rs
@@ -240,7 +240,7 @@ impl PathRect {
 
     #[cfg(all(
         not(target_arch = "wasm32"),
-        any(feature = "default", feature = "orbraq", feature = "miniraq"),
+        any(feature = "default", feature = "orbraq"),
     ))]
     /// Checks if the current instance is enclosed.
     pub fn get_clip(&self) -> Option<Rectangle> {
