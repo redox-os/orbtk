0901b9418d01b51dc2ea5f5c08b5abb43142e8dc
Ralf Zerres <ralf.zerres@networkx.de>
2021-03-06 12:38:50 +0100

    lint: apply suggested use of `unwarp_or(0)`
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/utils/build.rs
+++ crates/utils/build.rs
@@ -28,11 +28,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let clean_hex = hex.trim_start_matches('#');
         let color = match clean_hex.len() {
             3 => {
-                let d = match u32::from_str_radix(&clean_hex, 16) {
-                    Ok(x) => x,
-                    Err(_) => 0,
-                };
-
+                let d = u32::from_str_radix(&clean_hex, 16).unwrap_or(0);
                 let mut r = (d & 0xF) << 4;
                 let mut g = ((d >> 4) & 0xF) << 4;
                 let mut b = ((d >> 8) & 0xF) << 4;
@@ -43,10 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 format!("Color::rgb({}, {}, {})", r, g, b)
             }
             6 => {
-                let x = match u32::from_str_radix(&clean_hex, 16) {
-                    Ok(x) => x,
-                    Err(_) => 0,
-                };
+                let x = u32::from_str_radix(&clean_hex, 16).unwrap_or(0);
 
                 format!(
                     "Color::rgb({}, {}, {})",
