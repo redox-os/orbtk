73416f91bec5ba8680368047ed68742bf6b9fb16
Ralf Zerres <ralf.zerres@networkx.de>
2021-03-15 15:51:02 +0100

    crates:widget:text_block: update documentation
    * enhance online documentation text
    * sort properties in lexical order
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/widgets/src/text_block.rs
+++ crates/widgets/src/text_block.rs
@@ -1,5 +1,11 @@
 use crate::{api::prelude::*, proc_macros::*, theme_default::prelude::*};
 
+// --- KEYS --
+
+pub static STYLE_TEXT_BLOCK: &str = "text_block";
+
+// --- KEYS --
+
 enum TextAction {
     Localize,
 }
@@ -47,17 +53,8 @@ impl State for TextBlockState {
 widget!(
     /// The `TextBlock` widget is used to draw text. It is not interactive.
     ///
-    /// **style:** `text-block`
+    /// **style:** `text_block`
     TextBlock<TextBlockState> {
-        /// Sets or shares the text property.
-        text: String,
-
-        /// If the `TextBlock` is localizable and the localized text is not empty, the localized_text will be drawn.
-        localized_text: String,
-
-        /// Sets or shares the water_mark text property.
-        water_mark: String,
-
         /// Sets or shares the foreground property.
         foreground: Brush,
 
@@ -67,22 +64,35 @@ widget!(
         /// Sets or shares the font property.
         font: String,
 
+        /// Defines if the text is localizable. If set to `false` the text will not be localized.
+        localizable: bool,
+
+
+        /// If the `TextBlock` is localizable and the localized text
+        /// is not empty, the localized_text will be drawn.
+        localized_text: String,
+
         /// Defines an extra offset that can be used to the text on x axis.
         offset: f64,
 
-        /// Defines if the text is localizable. If set to `false` the text will not be localized.
-        localizable: bool
+        /// Sets or shares the text property.
+        text: String,
+
+        /// Sets or shares the water_mark text property.
+        water_mark: String
+
     }
 );
 
 impl Template for TextBlock {
     fn template(self, id: Entity, _: &mut BuildContext) -> Self {
         self.name("TextBlock")
-            .text("")
+            .style(STYLE_TEXT_BLOCK)
             .foreground(colors::LINK_WATER_COLOR)
             .font_size(fonts::FONT_SIZE_12)
             .font("Roboto-Regular")
             .localizable(true)
+            .text("")
             .on_changed("text", move |ctx, _| {
                 ctx.send_message(TextAction::Localize, id)
             })
