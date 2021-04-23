7beef07c21476bf0be4031c4eaaadba9495855e0
Florian Blasius <flovanpt@posteo.de>
2021-04-23 10:30:34 +0200

    expand crates structure.

--- orbtk/src/lib.rs
+++ orbtk/src/lib.rs
@@ -1,15 +1,24 @@
 pub mod core {
+    //! Core components of OrbTk. Provides base elements to create a user interface.
     pub use orbtk_core::*;
 }
 
 pub mod orbclient {
+    //! Ready to run (desktop and web) wrapper for OrbTk based on OrbClient.
     pub use orbtk_orbclient::*;
 }
 
 pub mod widgets {
+    //! Default widget library of OrbTk.
     pub use orbtk_widgets::*;
+}
+
+pub mod tiny_skia {
+    //! 2D drawing.
+    pub use orbtk_tiny_skia::*;
 }
 
 pub use self::core::*;
 pub use self::orbclient::*;
+pub use self::tiny_skia::*;
 pub use self::widgets::*;
