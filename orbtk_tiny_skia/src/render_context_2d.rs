6d5530a7b5c726519fe8032b507f16b06b599cd5
Florian Blasius <flovanpt@posteo.de>
2021-04-28 13:17:19 +0200

    add draw_text to render context 2d.

--- orbtk_tiny_skia/src/render_context_2d.rs
+++ orbtk_tiny_skia/src/render_context_2d.rs
@@ -1,1 +1,14 @@
+use orbtk_core::*;
+
 pub struct RenderContext2D {}
+
+impl RenderContext2D {
+    pub fn draw_text(
+        &mut self,
+        text: impl Into<String>,
+        position: Point,
+        font_size: u32,
+        font_family: &str,
+    ) {
+    }
+}
