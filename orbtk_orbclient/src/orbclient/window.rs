c9e1597ca3b4af2432ca2ee8a061bf08980408ed
Florian Blasius <flovanpt@posteo.de>
2021-06-10 09:52:57 +0200

    ci fixes

--- orbtk_orbclient/src/orbclient/window.rs
+++ orbtk_orbclient/src/orbclient/window.rs
@@ -218,7 +218,7 @@ where
             }
         };
 
-        self.adapter.key_event(KeyEvent { key, text, state });
+        self.adapter.key_event(KeyEvent { state, key, text });
     }
 
     /// Updates the clipboard.
