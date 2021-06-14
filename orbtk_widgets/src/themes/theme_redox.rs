02d5e8aa8c0be3e441fa462e7d52506936ff2551
Ralf Zerres <ralf.zerres@networkx.de>
2021-06-14 22:55:21 +0200

    orbtk_widgets: further documentation string extensions
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- orbtk_widgets/src/themes/theme_redox.rs
+++ orbtk_widgets/src/themes/theme_redox.rs
@@ -1,15 +1,21 @@
+/*!
+
+This submodule provides the default theme resources of OrbTks redox theme dark and light.
+It provides fonts, icons and colors.
+
+ */
 use orbtk_core::theming::*;
 
 use super::theme_orbtk::{register_default_fonts, MATERIAL_ICONS};
 
-/// Resource file of redox theme
+/// Resource file of redox theme.
 pub const THEME_REDOX: &str = include_str!("../../assets/themes/redox/theme_redox.ron");
 
 /// The redox dark theme colors resource file.
 pub const THEME_REDOX_COLORS_DARK: &str =
     include_str!("../../assets/themes/redox/theme_redox_colors.ron");
 
-/// The font resources of the redox theme
+/// The font resources of the redox theme.
 pub const THEME_REDOX_FONTS: &str = include_str!("../../assets/themes/redox/theme_redox_fonts.ron");
 
 /// Returns the redox OrbTk theme.
