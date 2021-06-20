06eb218b252e31131bc7bf5226a147e7c398b07c
Ralf Zerres <ralf.zerres@networkx.de>
2021-06-20 20:57:33 +0200

    orbtk_widgets: text_box update
    * preset a `min_height` instead of a fixed `height`
      for text_box in theme_default.ron, to allow text box
      dimensioning for multi-line input.
    * redefine default property attributes
    * update and reorder preset theme color names
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- orbtk_widgets/src/themes/theme_orbtk/colors.rs
+++ orbtk_widgets/src/themes/theme_orbtk/colors.rs
@@ -1,5 +1,6 @@
+pub const BOMBAY_COLOR: &str = "#adb3B8";
+pub const BRIGHT_GRAY_COLOR: &str = "#3b434a";
+pub const DARK_GRAY_COLOR: &str = "#2B2D2F";
 pub const LINK_WATER_COLOR: &str = "#dfebf5";
 pub const LYNCH_COLOR: &str = "#647b91";
-pub const BOMBAY_COLOR: &str = "#adb3B8";
 pub const SLATE_GRAY_COLOR: &str = "#6c7a90";
-pub const BRIGHT_GRAY_COLOR: &str = "#3b434a";
